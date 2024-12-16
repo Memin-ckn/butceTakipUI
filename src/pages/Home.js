66bce80a093ae57b18ebac0f12103a5a79b8b405
MURAT CAN AKKOÇ <canmuratakkoc@gmail.com>
2024-12-16 16:07:45 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,39 +26,48 @@ const Home = () => {
   useEffect(() => {
     const fetchTotalWithCategory = async () => {
       const result = await getTotalWithCategory();
-      setExpensesWithCategory(await Promise.all(Object.entries(result?.data?.expenses || {}).map(async ([categoryId, amount]) => {
+  
+      const expenses = result?.data?.expenses || {};
+      const incomes = result?.data?.incomes || {};
+  
+      // Check if expenses or incomes are not empty before proceeding with Object.entries
+      setExpensesWithCategory(await Promise.all(Object.entries(expenses).map(async ([categoryId, amount]) => {
         const category = await getExpenseCategory(categoryId);
         const categoryName = category.data ? category.data.name : "?";
         return { categoryId, amount, category: categoryName };
       })));
-
-      setIncomesWithCategory(await Promise.all(Object.entries(result?.data?.incomes || {}).map(async ([categoryId, amount]) => {
+  
+      setIncomesWithCategory(await Promise.all(Object.entries(incomes).map(async ([categoryId, amount]) => {
         const category = await getIncomeCategory(categoryId);
         const categoryName = category.data ? category.data.name : "?";
         return { categoryId, amount, category: categoryName };
       })));
-
-      const mostValuedExpenseCategory = Object.keys(result?.data?.expenses).reduce((maxKey, currentKey) => {
-        return result?.data?.expenses[currentKey] > result?.data?.expenses[maxKey] ? currentKey : maxKey;
-      }, Object.keys(result?.data?.expenses)[0]);
-
-      const mostValuedExpenseAmount = result?.data?.expenses[mostValuedExpenseCategory];
-      const expenseCategory = await getExpenseCategory(mostValuedExpenseCategory);
-      const expenseCategoryName = expenseCategory.data ? expenseCategory.data.name : "?";
-      setMostValuedExpense({ category: expenseCategoryName, amount: mostValuedExpenseAmount });
-
-      const mostValuedIncomeCategory = Object.keys(result?.data?.incomes).reduce((maxKey, currentKey) => {
-        return result?.data?.incomes[currentKey] > result?.data?.incomes[maxKey] ? currentKey : maxKey;
-      }, Object.keys(result?.data?.incomes)[0]);
-
-      const mostValuedIncomeAmount = result?.data?.incomes[mostValuedIncomeCategory];
-      const incomeCategory = await getIncomeCategory(mostValuedIncomeCategory);
-      const incomeCategoryName = incomeCategory.data ? incomeCategory.data.name : "?";
-      setMostValuedIncome({ category: incomeCategoryName, amount: mostValuedIncomeAmount });
+  
+      if (Object.keys(expenses).length > 0) {
+        const mostValuedExpenseCategory = Object.keys(expenses).reduce((maxKey, currentKey) => {
+          return expenses[currentKey] > expenses[maxKey] ? currentKey : maxKey;
+        }, Object.keys(expenses)[0]);
+  
+        const mostValuedExpenseAmount = expenses[mostValuedExpenseCategory];
+        const expenseCategory = await getExpenseCategory(mostValuedExpenseCategory);
+        const expenseCategoryName = expenseCategory.data ? expenseCategory.data.name : "?";
+        setMostValuedExpense({ category: expenseCategoryName, amount: mostValuedExpenseAmount });
+      }
+  
+      if (Object.keys(incomes).length > 0) {
+        const mostValuedIncomeCategory = Object.keys(incomes).reduce((maxKey, currentKey) => {
+          return incomes[currentKey] > incomes[maxKey] ? currentKey : maxKey;
+        }, Object.keys(incomes)[0]);
+  
+        const mostValuedIncomeAmount = incomes[mostValuedIncomeCategory];
+        const incomeCategory = await getIncomeCategory(mostValuedIncomeCategory);
+        const incomeCategoryName = incomeCategory.data ? incomeCategory.data.name : "?";
+        setMostValuedIncome({ category: incomeCategoryName, amount: mostValuedIncomeAmount });
+      }
     };
-
+  
     fetchTotalWithCategory();
-  }, []);
+  }, []);  
 
   const totalIncome = data.incomeTotal;
   const totalExpense = data.expenseTotal;
